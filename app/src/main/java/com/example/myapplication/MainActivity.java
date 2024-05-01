1d586069ea8c159083dedd0b05da02374f35937a
AndresC1 <andrescornejolira@gmail.com>
2024-04-30 23:44:24 -0600

    feat: Call to activity Sign Up

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.content.Intent;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        findViewById(R.id.btnSignUp).setOnClickListener(v -> {
+            startActivity(new Intent(this, SignUp.class));
         });
     }
 }
