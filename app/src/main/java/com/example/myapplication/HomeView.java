6c097fe4ebcf588e739146b5e608184a1b586bc0
AndresC1 <andrescornejolira@gmail.com>
2024-05-12 13:33:59 -0600

    feat: integration ListView and SQLite with methods basic

--- app/src/main/java/com/example/myapplication/HomeView.java
+++ app/src/main/java/com/example/myapplication/HomeView.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -9,10 +10,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.myapplication.Repository.NoteDB;
+import com.example.myapplication.ViewModel.NotesModel;
+
 public class HomeView extends AppCompatActivity {
 
-    String dataExample[] = {"Note 1", "Note 2 Example"};
-
+    NotesModel notes[] = {
+        new NotesModel(1,"Note in DB", "This is a note example 1 in DB", "2021-09-01", ""),
+        new NotesModel(2,"Note in BD 2", "This is a note example 2", "2021-09-01", ""),
+        new NotesModel(3,"Frutas", "Manzana, Melon, Banano", "2021-09-01", ""),
+        new NotesModel(4,"Colores", "Rojo, Blanco, Azul", "2021-09-01", ""),
+        new NotesModel(5,"Componentes", "Mouse, Teclado, Monitor, CPU", "2021-09-01", "")
+    };
     ListView listView;
 
     @Override
@@ -26,8 +35,10 @@ public class HomeView extends AppCompatActivity {
             return insets;
         });
 
+        NoteDB noteDB = new NoteDB(getApplicationContext());
+
         listView = (ListView) findViewById(R.id.NotesListView);
-        CustomBaseAdapetr customBaseAdapter = new CustomBaseAdapetr(getApplicationContext(), dataExample);
+        CustomBaseAdapetr customBaseAdapter = new CustomBaseAdapetr(getApplicationContext(), noteDB.getAll());
         listView.setAdapter(customBaseAdapter);
     }
 }
