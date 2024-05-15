a643e6019acec9efa0fa249dd59c09388b00d2b9
AndresC1 <andrescornejolira@gmail.com>
2024-05-15 12:07:34 -0600

    Feat: created method for SignUp user

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -1,12 +1,18 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.Repository.UserDB;
+import com.example.myapplication.ViewModel.UserModel;
+import com.example.myapplication.ViewModel.ValidateModel;
 
 public class SignUp extends AppCompatActivity {
 
@@ -19,6 +25,32 @@ public class SignUp extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        findViewById(R.id.btnSignUp).setOnClickListener(v -> {
+            try{
+                String email = ((EditText) findViewById(R.id.txtEmailNewUser)).getText().toString();
+                String password = ((EditText) findViewById(R.id.txtPasswordNewUser)).getText().toString();
+                String confirmPassword = ((EditText) findViewById(R.id.txtConfirmPasswordNewUser)).getText().toString();
+                ValidateModel validateModel = new ValidateModel();
+                String[] fields = {email, password, confirmPassword};
+                if(validateModel.validateFields(fields)){
+                    Toast.makeText(getApplicationContext(), "Please fill all fields", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                UserModel newUser = new UserModel(0, email, password, confirmPassword);
+                String infoValidation = newUser.validateInformation();
+                if(infoValidation != null){
+                    Toast.makeText(getApplicationContext(), infoValidation, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                UserDB userDB = new UserDB(getApplicationContext());
+                userDB.insert(newUser);
+                Toast.makeText(getApplicationContext(), "User created successfully", Toast.LENGTH_SHORT).show();
+                finish();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
         });
     }
 }
--- app/src/main/java/com/example/myapplication/ViewModel/ValidateModel.java
+++ app/src/main/java/com/example/myapplication/ViewModel/ValidateModel.java
@@ -9,4 +9,24 @@ public class ValidateModel {
         }
         return false;
     }
+
+    public boolean validateEmail(String email){
+        return email.contains("@") && email.contains(".");
+    }
+
+    public boolean validatePassword(String password){
+        return password.length() >= 8;
+    }
+
+    public boolean validatePasswordMatch(String password, String confirmPassword){
+        return password.equals(confirmPassword);
+    }
+
+    public boolean validatePasswordDiferentEmail(String email, String password){
+        return !email.equals(password);
+    }
+
+    public boolean validateCharacters(String password){
+        return password.matches(".*[a-zA-Z].*") && password.matches(".*[0-9].*") && password.matches(".*[!@#$%^&*].*");
+    }
 }
