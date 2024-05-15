8e784c1a384c2fcce5e6f3fdc2dfb491624f70fa
AndresC1 <andrescornejolira@gmail.com>
2024-05-15 09:10:15 -0600

    feat: created model User

---
+++ app/src/main/java/com/example/myapplication/ViewModel/UserModel.java
@@ -0,0 +1,41 @@
+package com.example.myapplication.ViewModel;
+
+public class UserModel {
+    private int id;
+    private String username;
+    private String password;
+
+    public UserModel(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+}
